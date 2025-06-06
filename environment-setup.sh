7d263a8f83c86677afae40b5993408717316064e
Lucasion <lucasion@hotmail.com>
2025-06-06 18:54:06 +0200

    Added yay to the setup

--- environment-setup.sh
+++ environment-setup.sh
@@ -1,1 +1,17 @@
+#!/bin/bash
+# Must run as root
+set -e
+if [[ $EUID -ne 0 ]]; then
+  echo "This script must be run as root"
+  exit 1
+fi
+
 pacman -Syu hyprland hyprpaper waybar hypridle hyprlock wofi mako openssh kitty
+
+# Install yay
+git clone https://aur.archlinux.org/yay.git /tmp/yay
+cd /tmp/yay
+makepkg -si --noconfirm
+
+cd -
+
