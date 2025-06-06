d400236f80ab4a31e3a0661de720814733fa5e4f
Lucasion <lucasion@hotmail.com>
2025-06-06 20:17:14 +0200

    Add global setup script and update environment setup for package installations

--- environment-setup.sh
+++ environment-setup.sh
@@ -5,36 +5,21 @@ if [[ $EUID == 0 ]]; then
   exit 1
 fi
 
-BUILDDIR=~/.builds
-
-mkdir -p $BUILDDIR
-
-sudo pacman --noconfirm -Syu \
-  hyprland hyprpaper waybar hypridle hyprlock wofi mako kitty \
-  base-devel openssh || { echo "Failed to install required packages. Check your network connection or sudo privileges."; exit 1; }
-
 if [[ ! -d /tmp/yay ]]; then
   # Install yay
   git clone https://aur.archlinux.org/yay.git /tmp/yay
   cd /tmp/yay
   makepkg -si --noconfirm
   
-  cd $BUILDDIR
+  cd /
   
   # Clean up yay directory
   rm -rf /tmp/yay
 fi
 
-cd $BUILDDIR
-
-# Install anyrun (https://github.com/anyrun-org/anyrun)
-# Install Rust toolchain
-sudo pacman -S --noconfirm rustup
+# Install Rust toolchain for compiling
 rustup default stable
 
-git clone https://github.com/anyrun-org/anyrun && cd anyrun
-cargo build --release
-cargo install --path $BUILDDIR/anyrun/
-mkdir -p ~/.config/anyrun/plugins
-cp target/release/*.so ~/.config/anyrun/plugins
-cp examples/config.ron ~/.config/anyrun/config.ron
+# Install anyrun (https://github.com/anyrun-org/anyrun)
+yay -S --noconfirm \
+  anyrun-git
---
+++ global-setup.sh
@@ -0,0 +1,11 @@
+set -e
+if [[ $EUID != 0 ]]; then
+  echo "This script must be run as root. Please use sudo or switch to root user."
+  exit 1
+fi
+
+pacman --noconfirm -Syu \
+  hyprland hyprpaper waybar hypridle hyprlock wofi mako kitty \
+  base-devel rustup openssh \
+  pipewire wireplumber xdg-desktop-portal-hyprland
+rustup default stable
