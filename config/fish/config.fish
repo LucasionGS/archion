7e2417e314c8dcf2d09146a22879f247d2cde979
Lucasion <lucasion@hotmail.com>
2025-06-07 18:25:44 +0200

    Update fish config and keybinds; add xorg-xwayland to global setup

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -4,6 +4,11 @@ set -gx EDITOR nvim
 # bobthefish settings
 set -g theme_display_docker_machine yes
 
+# Path
+set -gx PATH $HOME/.local/bin $PATH
+set -gx PATH /var/lib/snapd/snap/bin $PATH
+
+
 if status is-interactive
     # Commands to run in interactive sessions can go here
     set fish_greeting
