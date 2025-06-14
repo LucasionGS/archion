069b1577e40df25bf83fee404deb6b53ac62549d
LucasionArchion <lucasion@hotmail.com>
2025-06-15 01:25:40 +0200

    prep for multiple slideshow backgrounds

--- config/archion/scripts/promptwallpaper.sh
+++ config/archion/scripts/promptwallpaper.sh
@@ -1,9 +1,17 @@
 # Ask for file path from Nautilus
-FILE_PATH=$(zenity --file-selection --title="Select a Wallpaper Image")
+FILE_PATHS=$(zenity --file-selection --multiple --title="Select a Wallpaper Image(s)" --file-filter="*.jpg *.jpeg *.png *.gif" --separator="|")
+
+# Check if the user selected any files
+if [ -z "$FILE_PATHS" ]; then
+  exit 1
+fi
+
+# Convert the selected file paths into an array
+IFS='|' read -r -a FILE_ARRAY <<< "$FILE_PATHS"
+# Select the first file from the array
+FILE_PATH="${FILE_ARRAY[0]}"
 
 if [ -n "$FILE_PATH" ]; then
   # Set the wallpaper using swww
   swww img "$FILE_PATH"
-# else
-#   zenity --error --text="No file selected. Please select a valid image file."
 fi
