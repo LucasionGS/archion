4e90ba1eb089bd031ab25997a49293c3844d102f
LucasionArchion <lucasion@hotmail.com>
2025-07-05 22:48:36 +0200

    Refactor full-live-setup.sh to streamline command execution with logical chaining

--- full-live-setup.sh
+++ full-live-setup.sh
@@ -2,12 +2,8 @@
 # Full setup going through live-setup.sh, global-setup.sh, graphics-driver-setup.sh, and environment-setup.sh for the initial user.
 
 loadkeys dk
-sudo bash live-setup.sh
-USERNAME=$(cat /tmp/initial_archion_username)
-
-# Run on arch-chroot
-arch-chroot /mnt bash /home/$USERNAME/archion/global-setup.sh
-arch-chroot /mnt bash /home/$USERNAME/archion/graphics-driver-setup.sh
-
-# Run environment-setup.sh for the initial user
+sudo bash live-setup.sh && \
+USERNAME=$(cat /tmp/initial_archion_username) && \
+arch-chroot /mnt bash /home/$USERNAME/archion/global-setup.sh && \
+arch-chroot /mnt bash /home/$USERNAME/archion/graphics-driver-setup.sh && \
 arch-chroot /mnt bash -c "sudo -u $USERNAME bash /home/$USERNAME/archion/environment-setup.sh"
