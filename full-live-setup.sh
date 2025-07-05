0d1c167442778effe7609af9aab6f06f6d58461d
LucasionArchion <lucasion@hotmail.com>
2025-07-06 00:40:00 +0200

    Refactor full-live-setup.sh to improve structure and readability by commenting out username prompt and ensuring proper reboot sequence.

--- full-live-setup.sh
+++ full-live-setup.sh
@@ -5,6 +5,13 @@ loadkeys dk
 source ./utils.sh || { echo "utils.sh not found. Run from the script directory."; exit 1; }
 sudo bash live-setup.sh --no-umount && \
 arch-chroot /mnt bash /archion/global-setup.sh && \
-arch-chroot /mnt bash /archion/graphics-driver-setup.sh && \
-prompt "What was the username you set for primary user?: " USERNAME && \
-arch-chroot /mnt bash -c "sudo -u $USERNAME bash /archion/environment-setup.sh"
+arch-chroot /mnt bash /archion/graphics-driver-setup.sh 
+# && \
+# prompt "What was the username you set for primary user?: " USERNAME && \
+# arch-chroot /mnt bash -c "sudo -u $USERNAME bash /archion/environment-setup.sh"
+
+# restart live boot into system
+echo "Setup complete. Rebooting..."
+sleep 2
+umount -R /mnt || { echo ""; exit
+reboot
