de4d79317c679b85d236e92bb7d8df669f5aa604
LucasionArchion <lucasion@hotmail.com>
2025-07-05 23:57:46 +0200

    Improve user prompt clarity in full-live-setup.sh

--- full-live-setup.sh
+++ full-live-setup.sh
@@ -2,8 +2,9 @@
 # Full setup going through live-setup.sh, global-setup.sh, graphics-driver-setup.sh, and environment-setup.sh for the initial user.
 
 loadkeys dk
+source ./utils.sh || { echo "utils.sh not found. Run from the script directory."; exit 1; }
 sudo bash live-setup.sh --no-umount && \
 arch-chroot /mnt bash /archion/global-setup.sh && \
 arch-chroot /mnt bash /archion/graphics-driver-setup.sh && \
-prompt "Username for primary user: " USERNAME && \
+prompt "What was the username you set for primary user?: " USERNAME && \
 arch-chroot /mnt bash -c "sudo -u $USERNAME bash /archion/environment-setup.sh"
