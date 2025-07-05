8ca69f671aac4bb853592b0d486fe990b431834e
LucasionArchion <lucasion@hotmail.com>
2025-07-05 23:02:46 +0200

    Debugging

--- full-live-setup.sh
+++ full-live-setup.sh
@@ -5,6 +5,8 @@ loadkeys dk
 sudo bash live-setup.sh && \
 USERNAME="$(cat /tmp/initial_archion_username)"
 
+echo "Username: $USERNAME"
+
 # Run on arch-chroot
 arch-chroot /mnt bash /home/$USERNAME/archion/global-setup.sh
 arch-chroot /mnt bash /home/$USERNAME/archion/graphics-driver-setup.sh
